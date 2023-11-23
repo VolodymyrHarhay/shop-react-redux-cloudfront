0efe898d5300efa2f9de57daabc8106fd62481bd
Volodymyr Harhay <harhayv@buildone.co>
2023-11-23 17:16:31 +0200

    task-9 added url for cart api(eb)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,9 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://p90e8of7eh.execute-api.eu-north-1.amazonaws.com",
   bff: "https://akj8ohr428.execute-api.eu-north-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // cart: "https://r9ggxwvz5b.execute-api.eu-north-1.amazonaws.com/dev/api",
+  cart: "http://volodymyrharhay-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
+  // cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
